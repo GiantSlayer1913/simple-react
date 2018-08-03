cdf9065d2d7ace66e6016cedf9e6eb8e2cc01471
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-08-03 16:09:14 -0400

    Added winning logic to game

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
       currentTurn: "X",
       board: [
         "", "", "", "", "", "", "", "", ""
-      ]
+      ],
+      winner: null
     }
   }
 
@@ -20,9 +21,23 @@ class App extends Component {
       this.state.board[index] = this.state.currentTurn
       this.setState({
         board: this.state.board,
-        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
+        winner: this.checkForWinner()
       })
     }
+  }
+
+  checkForWinner() {
+    var currentTurn = this.state.currentTurn
+    var symbols = this.state.board
+    var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    return winningCombos.find(function(combo) {
+      if(symbols[combo[0]] !=="" && symbols[combo[1]] !=="" && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
+        return currentTurn
+      } else {
+        return false
+      }
+    })
   }
 
   render() {
