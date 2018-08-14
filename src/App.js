8f05ce37e325cb6b5114a75fd8e87c16165bd136
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2018-08-13 20:05:22 -0400

    Committing changes before beginning new sign up component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './login.js';
 
 class App extends Component {
 
@@ -17,15 +16,6 @@ class App extends Component {
     }
   }
 
-// signUp() {
-//   return (
-//     this.state.login.signUp.email.indexOf(`@`) &&
-//     this.state.login.signUp.password === this.state.login.signUp.passwordConfirmation &&
-//     this.state.login.signUp.password.length > 8 &&
-//     this.state.login.signUp.passwordConfirmation.length > 8
-//   )
-// }
-
   handleClick(index) {
     if (this.state.board[index] === "") {
       this.state.board[index] = this.state.currentTurn
@@ -41,39 +31,30 @@ class App extends Component {
     var currentTurn = this.state.currentTurn
     var symbols = this.state.board
     var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    return winningCombos.find(function(combo) {
-      if(symbols[combo[0]] !=="" && symbols[combo[1]] !=="" && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
-        return currentTurn
-      } else {
-        return false
-      }
-    })
-  }
-
-  // newGame() {
-  // if (this.state.winner !== null) {
-  //   this.state.board.setState({
-  //     board = ""
-  //   })
-  // } else {
-  //   return false
-  // }
-  // }
+    return winningCombos.find(
+      function(combo) {
+        if(symbols[combo[0]] !=="" && symbols[combo[1]] !=="" && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
+          return currentTurn
+        } else {
+          return false
+        }
+      })
+    }
 
   render() {
     return (
       <div className="App-container">
           <h1 className="App-title">Tic Tac Toe</h1>
-          <form className="Login">Login</form>
 
-          {this.state.winner ? <h2>{`The winner is ${this.state.winner}`}</h2> : null}
+
+          {this.state.winner ? <h2>{`The winner is in spaces ${this.state.winner}`}</h2> : null}
             <div className="board">
             {this.state.board.map((cell, index) => {
               return <div onClick={() => this.handleClick(index)} className="square">{cell}</div>;
             })}
             </div>
-            <div>
-            <button className="btn-primary">New Game</button>
+            <div className="newGame">
+            <button className="btn btn-primary">New Game</button>
             {this.newGame ? <h3>{`Sorry, you must finish the game before playing a new one.`}</h3> : null}
             </div>
             </div>
